f51e935154679440a3c4be6d72dc20f8123ebef7
0102-yang <1657375983@qq.com>
2021-07-13 18:17:40 +0800

    Add spring boot framework

---
+++ src/main/java/cn/yangliuqing/todolistbackend/TodoListBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.yangliuqing.todolistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author yang */
+@SpringBootApplication
+public class TodoListBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListBackendApplication.class, args);
+    }
+}
