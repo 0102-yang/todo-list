f8cdf41c9d25db619b87b59f0665a2c3ce102d17
0102-yang <1657375983@qq.com>
2021-07-19 21:55:53 +0800

    添加EntityScan

--- src/main/java/cn/yangliuqing/todolistbackend/TodoListBackendApplication.java
+++ src/main/java/cn/yangliuqing/todolistbackend/TodoListBackendApplication.java
@@ -2,11 +2,14 @@ package cn.yangliuqing.todolistbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /** @author yang */
 @SpringBootApplication
 @EnableScheduling
+@EntityScan(basePackageClasses = {TodoListBackendApplication.class, Jsr310JpaConverters.class})
 public class TodoListBackendApplication {
 
     public static void main(String[] args) {
